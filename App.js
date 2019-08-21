8654dd3bb840c7f5422b630caae26966d7dc235b
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2019-08-21 13:43:33 +0100

    add new components and functionality to app

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import Main from './app/Main';
+export default class App extends React.Component {
+  render() {
+    return <Main />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
